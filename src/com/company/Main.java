8acadf431de7c67ec15db34d7acf327a21b88067
Luke <Luke@lukes-mbp.home>
2021-01-12 17:30:34 +0000

    did Q1 from Project Euler

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(compute());
+
     }
+
+    public static String compute() {
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0)
+                sum += i;
+        }
+        return Integer.toString(sum);
+    }
+
 }
+
+
